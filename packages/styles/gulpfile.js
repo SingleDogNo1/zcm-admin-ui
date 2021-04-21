795a2cbf390dd7e46a6faffd0f8f0cbf63c0db2c
lost-dream <13623673118@163.com>
2021-04-21 17:04:08 +0800

    .

--- packages/styles/gulpfile.js
+++ packages/styles/gulpfile.js
@@ -1,7 +1,7 @@
 'use strict'
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { series, src, dest } = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-dart-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
@@ -10,7 +10,7 @@ const noPrefixFile = /(index|base|display)/
 
 function compile() {
   return src('./src/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass.sync())
     .pipe(autoprefixer({ cascade: false }))
     .pipe(cssmin())
     .pipe(
